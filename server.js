f13f4cb12357a8dd626c540ed25ba4ffaa96dd29
yassinos26 <yassine.250mellouli@gmail.com>
2026-04-15 12:36:28 +0100

    deploy_error

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+// server.js
+import app from "./app.js";
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running");
 });
